16aba7b52c4436af119c76d30eb98793257bc330
K1NGAARON <gerardaaron1997@gmail.com>
2024-08-02 11:46:45 +0200

    changes

--- home/js/script.js
+++ home/js/script.js
@@ -55,7 +55,7 @@ let text3 = document.getElementById("animated-3");
 let animatedSection = document.getElementById("animated-section");
 
 function load() {
-  animate(text1, 0, 26000, 3000);
+  animate(text1, 0, 15000, 3000);
   animate(text2, 0, 167400, 3000);
   animate(text3, 0, 2100, 3000);
 }

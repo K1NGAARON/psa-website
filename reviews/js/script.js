ad42ddb2f84e53721ed52b044c3023a3fd849041
K1NGAARON <gerardaaron1997@gmail.com>
2025-01-07 21:21:56 +0100

    changes

--- reviews/js/script.js
+++ reviews/js/script.js
@@ -3,7 +3,7 @@ const galleryWrapper = document.querySelector('#gallery');
 const reviews = []
 
 // Size of Reviews Folder (+1)
-const reviewsLength = 56;
+const reviewsLength = 64;
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {

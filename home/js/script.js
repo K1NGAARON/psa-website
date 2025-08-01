8588a5bd759d1b72ec6902890a877e0d9b303852
K1NGAARON <gerardaaron1997@gmail.com>
2025-08-01 15:27:04 +0200

    chanegs

--- home/js/script.js
+++ home/js/script.js
@@ -56,7 +56,7 @@ let animatedSection = document.getElementById("animated-section");
 
 function load() {
   animate(text1, 0, 15000, 3000);
-  animate(text2, 0, 210000, 3000);
+  animate(text2, 0, 400000, 3000);
   animate(text3, 0, 763, 3000);
 }
 
